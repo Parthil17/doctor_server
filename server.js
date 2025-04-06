ca16a3e0213c00b08a66ce13dff706241a1639e0
Parthil17 <parthilpanchani94@gmail.com>
2025-04-06 22:51:48 +0530

    first commmit

---
+++ server.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import morgan from 'morgan'
+import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
+import doctorRouter from './routes/doctorRoute.js'
+import userRouter from './routes/userRoute.js'
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary()
+
+// middlewares
+app.use(express.json())
+app.use(morgan("dev"))
+const corsOptions = {
+  origin: ['https://doctoradmin-ten.vercel.app','https://doctoruser.vercel.app'],  // or use a function to allow multiple origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], // allowed HTTP methods
+  credentials: true, // allow cookies and auth headers
+};
+app.use(cors(corsOptions))
+
+// api end point
+app.use('/api/admin', adminRouter)
+app.use('/api/doctor', doctorRouter)
+app.use('/api/user', userRouter)
+
+
+app.get('/', (req, res) => {
+  res.send('Api working...')
+})
+
+app.listen(port, () => console.log('Server started', port)) 
